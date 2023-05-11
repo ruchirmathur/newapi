3754cc33e594148bc2ac5423622bc367e467b2e2
ruchirmathur <ruchirmat@gmail.com>
2023-05-10 17:04:36 -0700

    Delete src/main/java/com/app/dao directory

--- src/main/java/com/app/dao/MemberData.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import com.app.model.MemberSearchRequest;
-import com.app.model.MemberSearchResponse;
-
-public interface MemberData {
-	
-	public MemberSearchResponse searchMember(MemberSearchRequest memberSearchRequest);
-}
