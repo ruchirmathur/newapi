92efbb34150f6ee6adf149c35520f3aa6afb0656
ruchirmathur <ruchirmat@gmail.com>
2023-05-10 17:04:00 -0700

    Add files via upload

--- src/main/java/com/app/api/MemberSearchAPI.java
+++ src/main/java/com/app/api/MemberSearchAPI.java
@@ -1,6 +1,5 @@
 package com.app.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,15 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.app.dao.MemberData;
 import com.app.model.MemberSearchRequest;
 import com.app.model.MemberSearchResponse;
 
 @RestController
 public class MemberSearchAPI {
 	
-	@Autowired
-	private MemberData memberData;
 
 	
 	/**
@@ -29,7 +25,21 @@ public class MemberSearchAPI {
 	@RequestMapping(value = "/api/searchMember", method = RequestMethod.POST,produces = "application/json")
 	public ResponseEntity<MemberSearchResponse> searchPatient(@RequestBody MemberSearchRequest memberSearchRequest) {
 
-		MemberSearchResponse patientSearchResponse = memberData.searchMember(memberSearchRequest);
+		MemberSearchResponse patientSearchResponse = new MemberSearchResponse();
+		
+		patientSearchResponse.setMember_dental_plan_name("Dental Plan");
+    	
+		patientSearchResponse.setMember_first_name("TestFirstName");
+    	
+		patientSearchResponse.setMember_last_name("LastName");
+    	
+		patientSearchResponse.setMember_medical_plan_name("Medical Plan");
+    	
+		patientSearchResponse.setMember_pcp_name("PCPName");
+    	
+		patientSearchResponse.setMember_user_name("UserName");
+    	
+		patientSearchResponse.setMember_vision_plan_name("Vision Plan");
 
 		return new ResponseEntity<MemberSearchResponse>(patientSearchResponse, HttpStatus.OK);
 	}
